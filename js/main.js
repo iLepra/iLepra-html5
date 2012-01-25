4faedcecb3023ee3d98e8488394bba7cdf968eea
Tim Ermilov <yamalight@gmail.com>
2012-01-25 15:37:15 +0100

    auth done

--- js/main.js
+++ js/main.js
@@ -17,19 +17,6 @@ document.addEventListener('touchmove', preventDefaultScroll, false);
 window.addEventListener('load', function(){    
     $.ui.showMask();
 
-    var getLatestPosts = function(){
-		// on posts data
-		$(document).bind(iLepra.events.ready, function(event){
-			$(document).unbind(iLepra.events.ready);
-			
-			$.ui.loadContent("postsPage");
-			$.ui.clearHistory();
-		});
-		
-		// get posts
-		iLepra.getLastPosts();
-	}
-
 	// bind event listener for initialization
 	$(document).bind(iLepra.events.init, function(event){
 		$(document).unbind(iLepra.events.init);
@@ -46,44 +33,59 @@ window.addEventListener('load', function(){
 			getLatestPosts();
 		}
 	});
-		
-    // bind yarr click
-    $("#loginButton").bind('tap', function(){
-        $.ui.showMask();
-    
-        // create auth data structure
-        var data = {
-            user: $("#username").val(),
-            pass: $("#pass").val(),
-            captcha: $("#captcha").val(),
-            logincode: iLepra.loginCode,
-            save: $("#rememberme")[0].checked ? 1 : 0
-        };
-        
-        // on login error
-        $(document).bind(iLepra.events.error, function(event){
-            $(document).unbind(iLepra.events.error);
-            // remove loader
-            $.ui.hideMask();
-            // display error message
-            iLepra.ui.showError(iLepra.errorMessage);
-            
-            // refresh captcha
-            $("#captchaImage").attr('src', iLepra.captchaURL);
-        });
-        
-        // prepare ready event
-        $(document).bind(iLepra.events.ready, function(event){
-            $(document).unbind(iLepra.events.ready);
-            $(document).unbind(iLepra.events.error);
-            // get posts
-            getLatestPosts();
-        });
-        
-        // try logging in
-        iLepra.tryLogin(data);
-    });
 	
 	// Init iLepra class
 	iLepra.init();
 });
+
+// get latest posts function
+var getLatestPosts = function(){
+    // on posts data
+    $(document).bind(iLepra.events.ready, function(event){
+        $(document).unbind(iLepra.events.ready);
+        
+        $.ui.loadContent("postsPage");
+        $.ui.clearHistory();
+    });
+    
+    // get posts
+    iLepra.getLastPosts();
+}
+
+
+ // yarr click function
+var tryLogin = function(){
+    $.ui.showMask();
+
+    // create auth data structure
+    var data = {
+        user: $("#username").val(),
+        pass: $("#pass").val(),
+        captcha: $("#captcha").val(),
+        logincode: iLepra.loginCode,
+        save: $("#rememberme")[0].checked ? 1 : 0
+    };
+    
+    // on login error
+    $(document).bind(iLepra.events.error, function(event){
+        $(document).unbind(iLepra.events.error);
+        // remove loader
+        $.ui.hideMask();
+        // display error message
+        iLepra.ui.showError(iLepra.errorMessage);
+        
+        // refresh captcha
+        $("#captchaImage").attr('src', iLepra.captchaURL);
+    });
+    
+    // prepare ready event
+    $(document).bind(iLepra.events.ready, function(event){
+        $(document).unbind(iLepra.events.ready);
+        $(document).unbind(iLepra.events.error);
+        // get posts
+        getLatestPosts();
+    });
+    
+    // try logging in
+    iLepra.tryLogin(data);
+};
