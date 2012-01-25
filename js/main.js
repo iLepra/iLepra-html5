57d76550fe0c81cd4a70ca4769a3525e31c7e84c
Tim Ermilov <yamalight@gmail.com>
2012-01-25 15:25:34 +0100

    change framework to jqmobi

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
 // jqm presets
-$( document ).bind( "mobileinit", function() {
-	$.support.cors = true;
-    $.mobile.allowCrossDomainPages = true;
-    $.mobile.pushStateEnabled = false;
-    $.loadingMessage = "";
-});
+var init = function(){
+   $.ui.backButtonText="Назад";  
+   //$.useViewport(320,480);
+   $("#navbar").hide();
+};
+window.addEventListener("load",init,false);  
+	
+/* This code prevents users from dragging the page */
+var preventDefaultScroll = function(event) {
+    event.preventDefault();
+    window.scroll(0,0);
+    return false;
+};
+document.addEventListener('touchmove', preventDefaultScroll, false);
 
-// fired up when phonegap is ready - this requires a phonegap.js file :|
-//document.addEventListener("deviceready", function(){
-//$(document).ready(function(){
-window.addEventListener('load', function(){
-    $.mobile.page.prototype.options.backBtnText = "Назад";
-    
-    var getLatestPosts = function(fromIndex){
-	    if( typeof fromIndex == 'undefined' ) fromIndex = false;
+window.addEventListener('load', function(){    
+    $.ui.showMask();
+
+    var getLatestPosts = function(){
 		// on posts data
 		$(document).bind(iLepra.events.ready, function(event){
-			// unbind
-			$(document).unbind(event);
+			$(document).unbind(iLepra.events.ready);
 			
-			var add = fromIndex ? "pages/" : "";
-			$.mobile.changePage(add+"posts.html");
+			$.ui.loadContent("postsPage");
+			$.ui.clearHistory();
 		});
 		
 		// get posts
@@ -29,64 +32,57 @@ window.addEventListener('load', function(){
 
 	// bind event listener for initialization
 	$(document).bind(iLepra.events.init, function(event){
-		$(document).unbind(event);
+		$(document).unbind(iLepra.events.init);
 		
 		if(!iLepra.isAuthenticated){
 			// navigate to login page
-			$.mobile.changePage("pages/login.html");
+			$.ui.loadContent("loginPage");
+			$.ui.clearHistory();
+			
+			// load captcha
+	        $("#captchaImage").attr('src', iLepra.captchaURL);
 		}else{
 			// get posts
-			getLatestPosts(true);
+			getLatestPosts();
 		}
 	});
-	
-	$("#loginPage").live("pagecreate", function(){
-		// load captcha
-		$("#captchaImage").attr('src', iLepra.captchaURL);
 		
-		// bind yarr click
-		$("#loginButton").bind('vclick', function(){
-            $.mobile.showPageLoadingMsg();
+    // bind yarr click
+    $("#loginButton").bind('tap', function(){
+        $.ui.showMask();
+    
+        // create auth data structure
+        var data = {
+            user: $("#username").val(),
+            pass: $("#pass").val(),
+            captcha: $("#captcha").val(),
+            logincode: iLepra.loginCode,
+            save: $("#rememberme").is(":checked") ? 1 : 0
+        };
         
-            // create auth data structure
-            var data = {
-                user: $("#username").val(),
-                pass: $("#pass").val(),
-                captcha: $("#captcha").val(),
-                logincode: iLepra.loginCode,
-                save: $("#rememberme").is(":checked") ? 1 : 0
-            };
+        // on login error
+        $(document).bind(iLepra.events.error, function(event){
+            $(document).unbind(iLepra.events.error);
+            // remove loader
+            $.ui.hideMask();
+            // display error message
+            iLepra.ui.showError(iLepra.errorMessage);
             
-            // on login error
-            $(document).bind(iLepra.events.error, function(event){
-                // unbind
-                $(document).unbind(event);
-                
-                // remove loader
-                $.mobile.hidePageLoadingMsg();
-                
-                // display error message
-                iLepra.ui.showError(iLepra.errorMessage);
-                
-                // refresh captcha
-                $("#captchaImage").attr('src', iLepra.captchaURL);
-            });
-            
-            // prepare ready event
-            $(document).bind(iLepra.events.ready, function(event){
-                // unbind current event
-                $(document).unbind(event);
-                // unbind error event
-                $(document).unbind(iLepra.events.error);
-                
-                // get posts
-                getLatestPosts();
-            });
-            
-            // try logging in
-            iLepra.tryLogin(data);
+            // refresh captcha
+            $("#captchaImage").attr('src', iLepra.captchaURL);
         });
-	});
+        
+        // prepare ready event
+        $(document).bind(iLepra.events.ready, function(event){
+            $(document).unbind(iLepra.events.ready);
+            $(document).unbind(iLepra.events.error);
+            // get posts
+            getLatestPosts();
+        });
+        
+        // try logging in
+        iLepra.tryLogin(data);
+    });
 	
 	// Init iLepra class
 	iLepra.init();
