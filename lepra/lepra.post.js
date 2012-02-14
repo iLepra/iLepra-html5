b8919f65d43ba26a637fd2d23d2e60cea23d3e69
Tim Ermilov <yamalight@gmail.com>
2012-02-14 21:06:19 +0100

    fix comments regex for sublepras

--- lepra/lepra.post.js
+++ lepra/lepra.post.js
@@ -2,16 +2,16 @@ iLepra.post = (function() {
     return {
         // current post
         current: null,
-        
+
         // comments array
         comments: null,
         newComments: null,
-        
+
         // get post comments
         getComments: function(id, type){
             if( typeof id == 'undefined' ) id = iLepra.post.current.id;
             if( typeof type == 'undefined' ) type = iLepra.post.current.type;
-            
+
             var url;
             if( type == 'inbox' ){
                 url = "http://leprosorium.ru/my/inbox/"+id
@@ -20,37 +20,37 @@ iLepra.post = (function() {
                 if( iLepra.post.current.domain_url.length > 0 ) server = iLepra.post.current.domain_url;
                 url = "http://"+server+"/comments/"+id
             }
-            
+
             $.get(url, function(data){
                 // cleanup data
                 data = data.replace(/\n+/g, '');
                 data = data.replace(/\r+/g, '');
                 data = data.replace(/\t+/g, '');
-                
+
                 iLepra.post.comments = [];
                 iLepra.post.newComments = [];
-                
+
                 var voteRes = /wtf_vote = '(.+?)'/gi.exec(data);
-                iLepra.post.current.wtf = { 
+                iLepra.post.current.wtf = {
                     comment: /<input type="hidden" name="wtf" value="(.+?)" \/>/g.exec(data)[1],
                     vote: voteRes != null ? voteRes[1] : null
                 }
-                
-                var commentReg = /<div id="(.+?)" class="post tree(.+?)"><div class="dt">(.+?)<\/div>.+?Написал.+?<a href="\/users\/.+?">(.+?)<\/a>,(.+?)<span>.+?<div class="vote".+?><em>(.+?)<\/em><\/span><a href="#".+?class="plus(.*?)">.+?<a href="#".+?class="minus(.*?)">/g;
-            
+
+                var commentReg = /<div id="(.+?)" class="post tree(.+?)"><div class="dt">(.+?)<\/div>.+?Написал.+?<a href=".*?\/users\/.+?">(.+?)<\/a>,(.+?)<span>.+?<div class="vote".+?><em>(.+?)<\/em><\/span><a href="#".+?class="plus(.*?)">.+?<a href="#".+?class="minus(.*?)">/g;
+
                 data = data.substr( data.indexOf('id="js-commentsHolder"') );
                 var res = commentReg.exec(data);
-                
+
                 var vote = 0;
-                if(res[7].length > 0){
+                if(res[7] != null && res[7].length > 0){
                     vote = 1;
-                }else if(res[8].length > 0){
+                }else if(res[8] != null && res[8].length > 0){
                     vote = -1;
                 }
-                
+
                 while(res != null){
                     var text = res[3];
-                    
+
                     // replace images with compressed ones
                     var imgReg = /img src="(.+?)"/g
                     var resImg = imgReg.exec(text);
@@ -58,11 +58,11 @@ iLepra.post = (function() {
                         text = text.replace(resImg[1], "http://src.sencha.io/"+iLepra.config.screenBiggest+"/"+resImg[1]);
                         resImg = imgReg.exec(text);
                     }
-                    
+
                     text = text.replace(/<p.*?>/gi, '');
                     text = text.replace(/<\/p>/gi, '');
                     text = text.replace(/<nonimg/ig, '<img');
-                
+
                     var post = {
                         id: res[1],
                         isNew: res[2].indexOf('new') != -1 ? 1 : 0,
@@ -72,18 +72,18 @@ iLepra.post = (function() {
                         when: res[5],
                         vote: vote
                     };
-                    
+
                     iLepra.post.comments.push(post);
                     if(post.isNew) iLepra.post.newComments.push(post);
-                    
+
                     res = commentReg.exec(data);
                 }
-                
+
                 // dispatch event
                 $(document).trigger(iLepra.events.ready);
             });
         },
-        
+
         // add comment to post
         addComment: function(comment, inReplyTo){
             var url = "http://";
@@ -93,18 +93,18 @@ iLepra.post = (function() {
                 url += "leprosorium.ru";
             }
             url += "/commctl/";
-            
+
             var data = {
                 wtf: iLepra.post.current.wtf.comment,
                 pid: iLepra.post.current.id,
                 comment: comment
             }
-            
+
             if(typeof inReplyTo != 'undefined' && inReplyTo != null) data.replyto = inReplyTo;
-            
+
             $.post(url, data, function(data){
                 data = $.parseJSON(data);
-                
+
                 if( data.status == "ERR" ){
                     $(document).trigger(iLepra.events.error);
                 }else{
@@ -112,7 +112,7 @@ iLepra.post = (function() {
                 }
             });
         },
-        
+
         // vote for comment
         voteComment: function(commentId, value){
             var url = "http://";
@@ -122,7 +122,7 @@ iLepra.post = (function() {
                 url += "leprosorium.ru";
             }
             url += "/rate/";
-            
+
             var data = {
                 type: 0,
                 wtf: iLepra.post.current.wtf.vote,
@@ -130,7 +130,7 @@ iLepra.post = (function() {
                 id: commentId,
                 value: value // 1 || -1
             }
-            
+
             // post
             $.post(url, data, function(data){});
         }
