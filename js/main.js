491324d8eb177babb898756fc80a22942153c91f
Tim Ermilov <yamalight@gmail.com>
2012-04-03 16:18:01 +0200

    open external links in new window (todo: add platform specific stuff in branches)

--- js/main.js
+++ js/main.js
@@ -12,6 +12,14 @@ window.addEventListener('load', function(){
     $.mobile.fixedtoolbar.prototype.options.tapToggle = false;
     //$.mobile.defaultPageTransition = 'none';
     //$.mobile.defaultDialogTransition = 'none';
+    $(document).on(iLepra.config.defaultTapEvent, "a", function(e){
+        var link = $(this).attr('href');
+        if(link.indexOf('http://') != -1){
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            window.open(link);
+        }
+    });
 
     ////////
     var getLatestPosts = function(fromIndex){
