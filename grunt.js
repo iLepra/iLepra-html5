c94639ac51e2a40b5325a77f31239eb1ae06f061
Tim Ermilov <yamalight@gmail.com>
2012-04-07 18:52:35 +0200

    fix grunt file

--- grunt.js
+++ grunt.js
@@ -11,7 +11,7 @@ grunt.initConfig({
         },
         logic: {
             src: ['js-src/js/templates.js', 'js-src/js/device.js', 'js-src/js/main.js', 'js-src/js/counters.js', 'js-src/js/handlers.js',
-                  'js-src/js/mystuff.js', 'js-src/js/inbox.js', 'js-src/js/navbar.js', 'js-src/js/posts.js', 'js-src/js/comments.js',
+                  'js-src/js/mystuff.js', 'js-src/js/inbox.js', 'js-src/js/posts.js', 'js-src/js/comments.js',
                   'js-src/js/more.js', 'js-src/js/favs.js', 'js-src/js/profile.js', 'js-src/js/subs.js', 'js-src/js/gov.js', 'js-src/js/chat.js'],
             dest: 'js-src/logic.js'
         }
